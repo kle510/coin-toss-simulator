db60aade997cd9a145079faa5582bef40631dc38
kle510 <khoi1092@gmail.com>
2020-05-23 16:58:34 -0700

    cleanup Bar

--- Bar.java
+++ Bar.java
@@ -1,8 +1,3 @@
-/* NAME: Kevin Le
- * September 2016
- */
-
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
@@ -19,49 +14,20 @@ public class Bar {
 	private Color barColor;
 	private String barTitle;
 
-	/**
-	 * Creates a labeled bar. 
-	 * 
-	 * @param bottom
-	 *            location of the bottom of the label
-	 * @param left
-	 *            location of the left side of the bar
-	 * @param width
-	 *            width of the bar
-	 * @param numUnits
-	 *            height of the bar in application units
-	 * @param unitsPerPixel
-	 *            how many application units per pixel for height
-	 * @param color
-	 *            the color of the bar
-	 * @param label
-	 *            the label at the bottom of the bar
-	 */
-	
 	public Bar(int bottom, int left, int width, int numUnits, double unitsPerPixel, Color color, String label) {
-
 		barBottom = bottom;
 		leftSideBar = left;
 		barWidth = width;
 		barHeight = numUnits;
 		barColor = color;
 		barTitle = label;
-
 	}
 
-	/*
-	 * Draws and sets the specified positioning and sizing of the bar. 
-	 * Fills the bar with specified color.
-	 * Creates and sets the specified positioning of the bar label. 
-	 */
 	public void draw(Graphics2D g2) {
-
-
 		Rectangle rectangle = new Rectangle(leftSideBar - barWidth / 2, barBottom - barHeight, barWidth, barHeight);
 		g2.setColor(barColor);
 		g2.draw(rectangle);
 		g2.fill(rectangle);
-
 
 		Font font = g2.getFont();
 		FontRenderContext context = g2.getFontRenderContext();
@@ -70,6 +36,5 @@ public class Bar {
 		int heightOfLabel = (int) labelBounds.getHeight();
 		g2.setColor(Color.BLACK);
 		g2.drawString(barTitle, leftSideBar - widthOfLabel / 2, barBottom + heightOfLabel);
-
 	}
 }
